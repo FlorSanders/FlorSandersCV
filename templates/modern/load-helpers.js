8fcfde505db3e9cc51f05a2b2a3517337f6f225e
Flor Sanders <me@florsanders.be>
2022-07-02 12:14:18 +0200

    Put all template properties in one file, remove tailwind dependency

--- templates/modern/load-helpers.js
+++
@@ -1,9 +0,0 @@
-const hbs = require("handlebars");
-const fs = require("fs");
-
-const partials = ["section", "column", "columns", "list"];
-
-for (const partial of partials) {
-  const content = fs.readFileSync(`./templates/modern/${partial}.hbs`, "utf-8");
-  hbs.registerPartial(partial, content);
-}
